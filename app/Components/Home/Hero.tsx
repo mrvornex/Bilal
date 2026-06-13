8813c3acd6c7869ab05c5335a53884899e4e7f7c
mrvornex <bilalali.office.pk@gmail.com>
2026-06-13 11:46:28 +0500

    Update

--- app/Components/Home/Hero.tsx
+++ app/Components/Home/Hero.tsx
@@ -2,7 +2,7 @@
 // import githubImage from "/images/bg2.png";
 // import resume from './public/Muhammad-Bilal.pdf';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin , faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 
@@ -14,10 +14,10 @@ const Hero = () => {
                     <div className="w-full h-full bg-dark-bg rounded-full">
                         <img
                             src="/Home_Img/bg9.png"
-                            alt="Muhammad Anas"
+                            alt="Muhammad Bilal"
                             className="w-full h-full object-cover rounded-full border-4 border-white"
                         />
-                    </div> 
+                    </div>
                 </div>
             </div>
 
@@ -39,7 +39,7 @@ const Hero = () => {
                         rel="noopener noreferrer"
                         className="hover:text-white transition-colors"
                     >
-                        <FontAwesomeIcon icon={faYoutube} size="1x" color="white"/> YouTube
+                        <FontAwesomeIcon icon={faYoutube} size="1x" color="white" /> YouTube
                     </a>
 
                     <div className="flex items-center gap-5 text-base max-md:mt-3 m-2">
@@ -49,8 +49,8 @@ const Hero = () => {
                             rel="noopener noreferrer"
                             className="flex items-center justify-center gap-1 hover:text-white transition-colors"
                         >
-                            <FontAwesomeIcon icon={faLinkedin} size="1x" color="white"/>
-                             LinkedIn
+                            <FontAwesomeIcon icon={faLinkedin} size="1x" color="white" />
+                            LinkedIn
                         </a>
                         <a
                             href="https://github.com/mrvornex"
@@ -59,11 +59,11 @@ const Hero = () => {
                             className="flex items-center justify-center gap-1 hover:text-white transition-colors"
                         >
                             {/* <img src={"bg1.svg"} alt="GitHub" className="w-5 h-5" /> */}
-                            <FontAwesomeIcon icon={faGithub} color="white"/>
+                            <FontAwesomeIcon icon={faGithub} color="white" />
                             GitHub
                         </a>
                         <a
-                            href={"/Muhammad-Bilal.pdf"}
+                            href={"/Muhammad Bilal.pdf"}
                             download="Muhammad-Anas-Resume.pdf"
                             className="px-4 py-2 border border-green-500 text-green-400 rounded-lg 
                  hover:bg-green-500/10 hover:shadow-[0_0_10px_rgba(34,197,94,0.7)] 
